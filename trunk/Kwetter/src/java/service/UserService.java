9f813b74176cfd0c23762b6cbdcc617075da618e
harrie.essing@gmail.com <harrie.essing@gmail.com@f48a5723-06ad-35ae-97cd-98edebd58890>
2011-06-07 13:02:23 +0000

    YEAH

--- trunk/Kwetter/src/java/service/UserService.java
+++ trunk/Kwetter/src/java/service/UserService.java
@@ -1,9 +1,10 @@
 package service;
 
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import domain.User;
 import domain.Tweet;
@@ -13,20 +14,54 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
-import javax.enterprise.inject.Alternative;
+import javax.inject.Inject;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 @Stateless
-public class UserService implements IUserService, Serializable  {
+public class UserService implements IUserService, Serializable {
 
     private List<User> users;
     //private Collection<Trend> trends;
     private HashMap<String, Long> trendMap;
+    // JMS zooi
+    private Context jndiContext;
+    private ConnectionFactory connectionFactory;
+    private Session session;
+    private Topic topic;
+    private Connection connection;
+    private MessageProducer producer;
 
-    public UserService() {
+    public UserService() throws NamingException, JMSException {
         users = new ArrayList();
         trendMap = new HashMap<String, Long>();
         //trends = new ArrayList();
         initUsers();
+
+        //JMS Producer
+        jndiContext = new InitialContext();
+        connectionFactory = (ConnectionFactory) jndiContext.lookup("MonitorConnectionFactory");
+        topic = (Topic) jndiContext.lookup("SearchTweetTopic");
+
+        /*connection = connectionFactory.createConnection();
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        System.out.println("voor producer aanmaken");
+        producer = session.createProducer(topic);
+        TextMessage message = session.createTextMessage("This is a textmessage");
+        message.setStringProperty("hashtag", "sport");
+        connection.start();
+        System.out.println("connection gestart");
+        producer.send(message);
+        connection.close();*/
     }
 
     @Override
@@ -51,14 +86,13 @@ public class UserService implements IUserService, Serializable  {
 
     @Override
     public User findUserByName(String name) {
-        for(User u: users) {
-            if(u.getName().equals(name)) {
+        for (User u : users) {
+            if (u.getName().equals(name)) {
                 return u;
             }
         }
         return null;
     }
-
 
     @Override
     public int count() {
@@ -78,16 +112,15 @@ public class UserService implements IUserService, Serializable  {
     @Override
     public Collection<Tweet> getSearchedTweets(String filter) {
         Collection<Tweet> temp = new ArrayList();
-        if(filter != null) {
+        if (filter != null) {
             //doorloop alle users
-            for(User u:users)
-            {
+            for (User u : users) {
                 //haal de tweets op bij User u
                 Collection<Tweet> tweets = u.getTweets();
                 //doorloop de tweets
-                for(Tweet t: tweets) {
+                for (Tweet t : tweets) {
                     //als filter in de tweet staat, voeg hem toe aan de temp lijst
-                    if(t.getTweet().toLowerCase().contains(filter)) {
+                    if (t.getTweet().toLowerCase().contains(filter)) {
                         temp.add(t);
                     }
                 }
@@ -102,14 +135,31 @@ public class UserService implements IUserService, Serializable  {
         u.addTweet(new Tweet(tweet, new Date(), "PC"));
 
         String[] words = tweet.split(" ");
-        for(String s: words) {
-            if(s.startsWith("#")) {
-                if(trendMap.containsKey(s)) {
-                    trendMap.put(s, trendMap.get(s) + 1);
+        for (String s : words) {
+            if (s.startsWith("#")) {
+                try {
+                    connection = connectionFactory.createConnection();
+                    session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+                    System.out.println("voor producer aanmaken");
+                    producer = session.createProducer(topic);
+                    
+                    TextMessage message = session.createTextMessage(tweet);
+                    message.setStringProperty("hashtag", s.replace("#", ""));
+                    connection.start();
+                    System.out.println("connection gestart");
+                    producer.send(message);
+                    connection.close();
+
+                } catch (JMSException ex) {
+                    Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                else {
+                if (trendMap.containsKey(s)) {
+                    trendMap.put(s, trendMap.get(s) + 1);
+                } else {
                     trendMap.put(s, 1L);
                 }
+
             }
         }
     }
@@ -120,15 +170,14 @@ public class UserService implements IUserService, Serializable  {
         //sorteer de hashmap op value en doorloop hem
         Iterator i = sortByValue(trendMap).iterator();
         int teller = 0;
-        while(teller < 5) //haal de bovenste vijf trends op
+        while (teller < 5) //haal de bovenste vijf trends op
         {
-            if(i.hasNext()) {
+            if (i.hasNext()) {
                 temp.add((String) i.next());
-                teller ++;
-            }
-            else {
+                teller++;
+            } else {
                 break;
-            }            
+            }
         }
         return temp;
     }
@@ -144,11 +193,9 @@ public class UserService implements IUserService, Serializable  {
                 Object v2 = m.get(o2);
                 if (v1 == null) {
                     return (v2 == null) ? 0 : 1;
-                }
-                else if (v1 instanceof Comparable) {
+                } else if (v1 instanceof Comparable) {
                     return ((Comparable) v2).compareTo(v1);
-                }
-                else {
+                } else {
                     return 0;
                 }
 
@@ -158,11 +205,11 @@ public class UserService implements IUserService, Serializable  {
     }
 
     @Override
-    public void initUsers(){
-        User u1 = new User("hans","http://www.google.nl","geboren 1");
-        User u2 = new User("frank","httpF","geboren 2");
-        User u3 = new User("tom","httpT","geboren 3");
-        User u4 = new User("sjaak","httpS","geboren 4");
+    public void initUsers() {
+        User u1 = new User("hans", "http://www.google.nl", "geboren 1");
+        User u2 = new User("frank", "httpF", "geboren 2");
+        User u3 = new User("tom", "httpT", "geboren 3");
+        User u4 = new User("sjaak", "httpS", "geboren 4");
         User u5 = new User("default", "httpD", "geboren 5");
 
         u1.addFollowing(u2);
